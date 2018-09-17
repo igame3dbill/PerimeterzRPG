b17c2eb517f5b25411957214bbe43232c1e2823e
iGame3DBill <igame3dbill@gmail.com>
2018-09-16 20:54:00 -0600

    All Assets moved to iGame3DBill Git account
    10,000+ unnecessary sprites removed, awesome title sequence added, hopefully I didn't lose it in all this git shuffling, if I did I'm going to be very angry.

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+    [SerializeField] float speed = 5;
+    [SerializeField] Canvas battleGui;
+
+	// Use this for initialization
+	void Start () {
+        GameManager.INSTANCE.Events.OnUpdateEvent += OnUpdate;
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void OnUpdate () {
+        if (GameManager.INSTANCE.InGui)
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
+        else
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Vector2 velocity = new Vector2(h, v) * speed;
+            rigidbody.velocity = velocity;
+        }
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.INSTANCE.SetGui(battleGui);
+    }
+    public void DoAction(int actionID)
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        if(GameManager.INSTANCE != null)
+            GameManager.INSTANCE.Events.OnUpdateEvent -= OnUpdate;
+    }
+}
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    
+    /// <summary>
+    /// A holder for game events
+    /// </summary>
+    public class EventManager
+    {
+        public event Action OnUpdateEvent;
+        public event Action OnFixedUpdateEvent;
+        public event Action OnLateUpdateEvent;
+
+        public void OnUpdate()
+        {
+            if(OnUpdateEvent != null)
+                OnUpdateEvent();
+        }
+        public void OnLateUpdate()
+        {
+            if(OnFixedUpdateEvent != null)
+                OnLateUpdateEvent();
+        }
+        public void OnFixedUpdate()
+        {
+            if(OnFixedUpdateEvent != null)
+                OnFixedUpdateEvent();
+        }
+    }
+    //A static holder of the current GameManager
+    public static GameManager INSTANCE { get; private set; }
+    public EventManager Events { get; private set; }
+    public bool InGui { get; private set; }
+
+    public Canvas gui;
+
+	// Use this for preinitialization
+	void Awake () {
+        //Endure there is only ever 1 GameManager Active
+        if (INSTANCE != null)
+        {
+            Destroy(this);
+            return;
+        }
+        //Create Static Accessor to avoid FindGameObject calls
+        INSTANCE = this;
+        Events = new EventManager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Events.OnUpdate();
+	}
+    // LateUpdate is called at the end of every frame
+    void LateUpdate()
+    {
+        Events.OnLateUpdate();
+    }
+    //Called every fixed time setp
+    void FixedUpdate()
+    {
+        Events.OnFixedUpdate();
+    }
+
+    public void SetGui(Canvas gui)
+    {
+        if (this.gui != null)
+            CloseGui();
+        gui.gameObject.SetActive(true);
+        this.gui = gui;
+        InGui = true;
+    }
+    public void CloseGui()
+    {
+        gui.gameObject.SetActive(false);
+        gui = null;
+        InGui = false;
+    }
+
+    //Called when the gamemanager is destoryed (Is called after game ends)
+    void OnDestroy()
+    {
+        if (INSTANCE == this)
+            INSTANCE = null;
+    }
+}
